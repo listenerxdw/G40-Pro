36d9a7b3d79670d8f47ce60417985c29d180be03
seerwang <38456329+seerwang@users.noreply.github.com>
2018-10-21 07:19:08 +1100

    Add files via upload

---
+++ Instagram_Pro/Model/Follow.swift
@@ -0,0 +1,18 @@
+//
+//  Follow.swift
+//  InstagramFirebase
+//
+//  Created by Mengchen Wang on 10/21/18.
+//  Copyright © 2018 Lets Build That App. All rights reserved.
+//
+
+import Foundation
+struct Follow {
+    
+    let user: User
+    
+    
+    init(user: User) {
+        self.user = user
+    }
+}
