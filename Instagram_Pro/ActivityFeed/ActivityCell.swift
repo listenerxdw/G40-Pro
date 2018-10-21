5e9171d53d4dc0be53b2f74d8010cefff0ead93d
seerwang <38456329+seerwang@users.noreply.github.com>
2018-10-21 15:12:43 +1100

    Add files via upload
    Final

--- Instagram_Pro/ActivityFeed/ActivityCell.swift
+++ Instagram_Pro/ActivityFeed/ActivityCell.swift
@@ -2,40 +2,68 @@ import UIKit
 
 class  ActivityCell: UICollectionViewCell {
     
-    var follow: Follow?{
+    var post: Post?{
         didSet{
-            guard let follow = follow else {return}
-            usernameLabel.text = follow.user.username + " has followed you "
-            profileImageView.loadImage(urlString: follow.user.profileImageUrl)
+            guard let post = post else {return}
+            usernameLabel.text = post.user.username + " posts a picture "
+            profileImageView.loadImage(urlString: post.user.profileImageUrl)
+            postImageView.loadImage(urlString: post.imageUrl)
+            //timeLabel.text = post.creationDate.timeAgoDisplay()
+            timeLabel.text = post.creationDate.timeAgoDisplay()
         }
     }
     
     let profileImageView : CustomImageView = {
         let iv = CustomImageView()
-        iv.backgroundColor = .red
+        //iv.backgroundColor = .red
         iv.contentMode = .scaleAspectFill
         iv.clipsToBounds = true
         return iv
     }()
     
-    let usernameLabel : UILabel = {
+    let timeLabel : UILabel = {
         let label = UILabel()
-        label.text = "username"
-        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.text = "time"
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .gray
         return label
     }()
     
+    let usernameLabel : UILabel = {
+        let label = UILabel()
+        label.text = "username"
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        return label
+    }()
+    
+    let postImageView: CustomImageView={
+        let iv = CustomImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
     
     override init(frame:CGRect){
         super.init(frame:frame)
         backgroundColor = .white
         addSubview(profileImageView)
         addSubview(usernameLabel)
+        addSubview(postImageView)
+        addSubview(timeLabel)
         usernameLabel.anchor(top: topAnchor, left: profileImageView.rightAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        usernameLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
         
-        profileImageView.anchor(top: nil, left: leftAnchor, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 50, height: 50)
-        profileImageView.layer.cornerRadius = 50/2
+        timeLabel.anchor(top: nil, left: profileImageView.rightAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 8, paddingBottom: -25, paddingRight: 0, width: 0, height: 0)
+        
+        profileImageView.anchor(top: nil, left: leftAnchor, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 80, height: 80)
+        profileImageView.layer.cornerRadius = 80/2
         profileImageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        
+        
+        postImageView.anchor(top: nil, left: nil, bottom: nil, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 15, width: 80, height: 80)
+        //postImageView.layer.cornerRadius = 80/2
+        postImageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
         
         let seperatorView = UIView()
         seperatorView.backgroundColor = UIColor(white:0, alpha:0.5)
--- Instagram_Pro/ActivityFeed/ActivityFeed.swift
+++ Instagram_Pro/ActivityFeed/ActivityFeed.swift
@@ -23,55 +23,63 @@ class ActivityFeed: UICollectionViewController, UICollectionViewDelegateFlowLayo
         
         
         fetchActivity()
+        setupNavigationItems()
+    }
+    
+    func setupNavigationItems() {
+        navigationItem.title = "Notification"
+
     }
     
 
-    var follows = [Follow]()
-    
+    var posts = [Post]()
     private func fetchActivity(){
         guard let uid = Auth.auth().currentUser?.uid else { return }
-        let ref = Database.database().reference().child("follower").child(uid)
-            ref.observe(.childAdded, with: { (snapshot) in
-                //print(snapshot)
-                //guard let dictionary = [snapshot.key:snapshot.value] as? [String:Any] else{return}
-                let keys = snapshot.key
-                print(keys)
-                
-                Database.fetchUserWithUID(uid: keys, completion: { (user) in
-                    var follow = Follow(user: user)
-                    //follow = Follow(user: user)
-                    self.follows.append(follow)
-                    self.collectionView?.reloadData()
-                })
-                //print(snapshot)
-//            dictionary.forEach({ (key, value) in
-//                Database.fetchUserWithUID(uid: key, completion: { (user) in
-//
-//                })
-//            })
+        let ref = Database.database().reference().child("posts")
+        let ref2 = Database.database().reference().child("following").child(uid)
+        ref2.observe(.childAdded) { (snapshot2) in
+            guard let id = snapshot2.key as? String else {return}
+            //print(id)
             
-            //self.collectionView?.reloadData()
-        }) { (err) in
-            print(err)
+            ref.observe(.childAdded, with: { (snapshot) in
+                if(snapshot.key == id)
+                    {
+                        Database.fetchUserWithUID(uid: id, completion: { (user) in
+                        //print(user.username)
+                        guard let dictionary = snapshot.value as? [String:Any ] else {return}
+                        guard let newPost = dictionary.values.first! as? [String: Any] else {return}
+                            let constructDict = ["imageUrl":newPost["imageUrl"],"caption":newPost["caption"],"creationDate":newPost["creationDate"]]
+                        var post = Post(user: user, dictionary: constructDict)
+                        self.posts.append(post)
+                        self.collectionView?.reloadData()
+                        })
+
+                }
+                //print(dictionary)
+                //print(dictionary)
+                //print(dictionary[snapshot2.key])
+                
+            })
         }
+        
         
     
     }
 
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return follows.count
+        return posts.count
     }
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! ActivityCell
         cell.backgroundColor = .white
-        cell.follow = follows[indexPath.item]
+        cell.post = posts[indexPath.item]
         
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height:  60)
+        return CGSize(width: view.frame.width, height:  100)
     }
 
 }
